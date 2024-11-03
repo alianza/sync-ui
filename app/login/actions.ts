7c745878c0fc9784cece69ef10aa36cce135754e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-11-03 02:08:21 +0100

    Added login and logout actions

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,19 +1,52 @@
 "use server";
 
-export async function login(prevState: unknown, formData: FormData) {
-  const rawFormData = {
-    email: formData.get("email"),
-    password: formData.get("password"),
-  };
+import { signIn, signOut } from "@/auth";
 
-  console.log(`rawFormData`, rawFormData);
+// export async function login(prevState: unknown, formData: FormData) {
+//   const rawFormData = {
+//     email: formData.get("email"),
+//     password: formData.get("password"),
+//   };
+//
+//   console.log(`rawFormData`, rawFormData);
+//
+//   // Send the form data to the API or email service
+//   try {
+//     const user = await signIn("credentials", formData);
+//     const session = await auth();
+//     console.log(`session`, session);
+//     console.log(`user`, user);
+//     return {
+//       message: `Successfully logged in!`,
+//     };
+//   } catch (error) {
+//     return {
+//       message: `Invalid credentials. Please try again.`,
+//     };
+//   }
+//
+//   // revalidatePath("/pricing");
+//
+//   return {
+//     message: `Received the form data: ${JSON.stringify(rawFormData, null, 2)}`,
+//   };
+// }
 
-  // Send the form data to the API or email service
-  await new Promise((resolve) => setTimeout(() => resolve("resolved"), 1000)); // Simulate a delay
+export async function signInAction(formData: FormData) {
+  // try {
+  //   const user = await signIn("credentials", formData);
+  //
+  //   console.log(`actionUser`, user);
+  //
+  //   return user;
+  // } catch (error) {
+  //   return {
+  //     message: `Invalid credentials. Please try again.`,
+  //   };
+  // }
+  return signIn("credentials", formData);
+}
 
-  // revalidatePath("/pricing");
-
-  return {
-    message: `Received the form data: ${JSON.stringify(rawFormData, null, 2)}`,
-  };
+export async function signOutAction() {
+  return signOut();
 }
