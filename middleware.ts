301bd8350f90d5336c6573479649c7ed537b9907
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-11-03 02:16:45 +0100

    Disable middleware for now

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,3 @@
-export { auth as middleware } from "@/auth"; // import the auth middleware from the auth module
+// export { auth as middleware } from "@/auth"; // import the auth middleware from the auth module,
+// not possible for now due to mongoose not supporting edge runtime, and middleware only works in edge runtime
+// possible solution, user prismaOrm instead of mongoose to connect to the database instead here
