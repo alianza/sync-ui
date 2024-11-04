d56f61f4f3bedd0372659f7eaab5eea80ced1f28
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-11-04 05:01:54 +0100

    Expanded server.utils.ts with responses

--- lib/server.utils.ts
+++ lib/server.utils.ts
@@ -1,9 +1,38 @@
 import { HydratedDocument } from "mongoose";
+import { ResponseStatus } from "@/lib/types";
 
-export function serialize<T>(doc: HydratedDocument<T> | HydratedDocument<T>[]): T | T[] {
+export function serializeDoc<T>(doc: HydratedDocument<T> | HydratedDocument<T>[]): T | T[] {
   if (Array.isArray(doc)) {
     return doc.map((item) => item.toObject({ flattenObjectIds: true })) as T[];
   }
 
   return doc.toObject({ flattenObjectIds: true }) as T;
 }
+
+/**
+ * Success response with message and data
+ * @param message
+ * @param data
+ */
+export const successResponse = <T>({ message, data }: { message?: string; data?: T }) => ({
+  status: ResponseStatus.success,
+  message,
+  data,
+});
+
+/**
+ * Fail response with message and data
+ * @param message
+ * @param data
+ */
+export const failResponse = <T>({ message, data }: { message?: string; data?: T }) => ({
+  status: ResponseStatus.fail,
+  message,
+  data,
+});
+
+/**
+ * Error response with message
+ * @param message
+ */
+export const errorResponse = <T>(message: T) => ({ status: ResponseStatus.error, message: String(message) });
