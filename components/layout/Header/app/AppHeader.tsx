d0679f18e3658f9290746e6f5792243703a202c4
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-11-15 06:18:29 +0100

    Disable sidebar for now

--- components/layout/Header/app/AppHeader.tsx
+++ components/layout/Header/app/AppHeader.tsx
@@ -1,22 +1,22 @@
 import NavLink from "@/components/NavLink";
 import { HeaderLogo } from "@/components/layout/Header/HeaderLogo";
-import { auth } from "@/auth";
-import SignOutButton from "@/components/SignOutButton";
-import { DynamicHomeHeaderLinks } from "@/components/layout/Header/home/DynamicHomeHeaderLinks";
-import { homeMenuItems } from "@/components/layout/Header/home/HomeHeader";
 import { DynamicAppHeaderLinks } from "@/components/layout/Header/app/DynamicAppHeaderLinks";
 import { MobileNav } from "@/components/layout/Header/MobileNav";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@/components/ui/separator";
+import { Home, ScrollText } from "lucide-react";
 
-const appMenuItems = [{ href: "/dashboard/listings", label: "Listings" }];
+export const appMenuItems = [
+  { href: "/dashboard", label: "Dashboard", exact: true, icon: Home },
+  { href: "/dashboard/listings", label: "Listings", icon: ScrollText },
+];
 
 export async function AppHeader() {
   return (
     <header className="flex items-center bg-background p-4 shadow dark:border-b-2">
       <div className="flex h-full items-center gap-2">
-        <SidebarTrigger className="size-7" />
-        <Separator orientation="vertical" className="grow" />
+        {/*<SidebarTrigger className="size-7" />*/}
+        {/*<Separator orientation="vertical" className="grow" />*/}
         <HeaderLogo />
       </div>
 
@@ -33,8 +33,8 @@ export async function AppHeader() {
 function AppNavItems() {
   return (
     <>
-      {appMenuItems.map(({ href, label }, index) => (
-        <NavLink key={index} href={href} label={label} />
+      {appMenuItems.map(({ href, label, exact }, index) => (
+        <NavLink key={index} href={href} label={label} exact={exact} />
       ))}
       <DynamicAppHeaderLinks />
     </>
