6ee2afabd081dfdd0c976cfd5cf5413fc76425e1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-12-06 23:12:47 +0100

    Fix loader

--- app/(app)/dashboard/listings/loading.tsx
+++ app/(app)/dashboard/listings/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex grow items-center justify-center">
+    <div className="flex h-96 items-center justify-center">
       <div className="h-12 w-12 animate-spin rounded-full border-2 border-l-primary" />
     </div>
   );
