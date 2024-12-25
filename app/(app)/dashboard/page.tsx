27afb8b8440ece3021f99e6e95ce4f21db80e900
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-12-25 03:00:37 +0100

    Added front-end role test

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -14,7 +14,8 @@ export default async function Dashboard() {
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="px-4 md:px-6">
         <div className="flex flex-col items-center justify-center text-center">
-          Welcome to the dashboard, {getNameFromAlias(session.user.name!)}!
+          <h1 className="text-xl">Welcome to the dashboard, {getNameFromAlias(session.user.name!)}!</h1>
+          <p>Role: {session.user.role}</p>
         </div>
       </div>
     </section>
