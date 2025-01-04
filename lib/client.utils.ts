4439377760883917d5fc0706b860caea8ffd49b1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-04 23:49:00 +0100

    Allow handleAction from button event

--- lib/client.utils.ts
+++ lib/client.utils.ts
@@ -1,7 +1,14 @@
 import React, { startTransition } from "react";
 
-export const handleAction = (e: React.FormEvent<HTMLFormElement>, action: (data: FormData) => void) => {
+export const handleAction = (
+  e: React.FormEvent<HTMLFormElement | HTMLButtonElement>,
+  action: (data: FormData) => void,
+) => {
   e.preventDefault();
+  if (e.currentTarget instanceof HTMLButtonElement) {
+    startTransition(() => action(new FormData()));
+    return;
+  }
   const formData = new FormData(e.currentTarget);
   startTransition(() => action(formData)); // use onSubmit instead of action to retain the form state
 };
