be8709aaa97b422ac1f7aa76a265debb928d5338
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-04 23:50:35 +0100

    Only close mobile nav on route url param change

--- components/layout/Header/MobileNav.tsx
+++ components/layout/Header/MobileNav.tsx
@@ -3,18 +3,26 @@
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export function MobileNav({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    setIsOpen(false); // Close the menu when the route changes
+  }, [pathname, searchParams]);
 
   return (
     <Sheet open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
       <SheetTrigger asChild>
         <Button variant="ghost" className="ml-auto md:hidden" onClick={toggleMenu}>
-          <Menu className="h-6 w-6" />
+          <Menu className="size-6" />
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
@@ -23,9 +31,7 @@ export function MobileNav({ children }: { children: React.ReactNode }) {
         <SheetDescription className="sr-only">
           Navigate to different sections of the website using the links below.
         </SheetDescription>
-        <nav className="flex flex-col gap-2" onClick={toggleMenu}>
-          {children}
-        </nav>
+        <nav className="flex flex-col gap-2">{children}</nav>
       </SheetContent>
     </Sheet>
   );
