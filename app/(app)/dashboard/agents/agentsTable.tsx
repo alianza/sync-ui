60c672e7f55b4107b66daea2b126c7efa7437605
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-05 02:04:30 +0100

    Added agents and leads pages for buyer role

---
+++ app/(app)/dashboard/agents/agentsTable.tsx
@@ -0,0 +1,63 @@
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "@/app/(app)/dashboard/agents/columns";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import React from "react";
+import { redirect } from "next/navigation";
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+import { MergeType } from "mongoose";
+import { UserDoc } from "@/models/User.type";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { authGuard } from "@/lib/server.utils";
+
+export default async function AgentsTable() {
+  const session = await authGuard();
+
+  await dbConnect();
+  const dbUser = (await User.findById(session.user?.id).populate("clients"))?.toObject({
+    flattenObjectIds: true,
+  }) as MergeType<UserDoc, { clients: UserDoc[] }>;
+
+  if (!dbUser) redirect("/login");
+
+  const newClientButton = (
+    <Link href="/dashboard/clients/new">
+      <Button>
+        <PlusIcon /> Nodig nieuwe klant uit
+      </Button>
+    </Link>
+  );
+
+  return (
+    <div className="flex flex-col gap-2">
+      <DataTable
+        columns={columns}
+        data={dbUser.clients}
+        filterColumn="email"
+        filterPlaceholder="Filter klanten"
+        emptyComponent={
+          <div className="flex flex-col justify-center gap-2">
+            Geen resultaten gevonden.
+            {newClientButton}
+          </div>
+        }
+      />
+
+      {dbUser.clients.length > 0 ? (
+        <div className="self-start">{newClientButton}</div>
+      ) : (
+        <Card className="mx-auto">
+          <CardHeader>
+            <CardTitle>Voeg je eerste klant toe!</CardTitle>
+            <CardDescription>
+              Je hebt nog geen klanten toegevoegd. Klik op de knop hieronder om je eerste klant uit te nodigen!
+            </CardDescription>
+          </CardHeader>
+          <CardContent>{newClientButton}</CardContent>
+        </Card>
+      )}
+    </div>
+  );
+}
---
+++ app/(app)/dashboard/agents/columns.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown, Eye, MoreHorizontal, SortAsc, SortDesc, Trash } from "lucide-react";
+import Link from "next/link";
+import ConfirmDialog from "@/components/ConfirmDialog";
+import { toast } from "@/hooks/use-toast";
+import { ResponseStatus } from "@/lib/types";
+import { UserDoc } from "@/models/User.type";
+import { deleteClient } from "@/app/(app)/dashboard/clients/actions";
+
+export const columns: ColumnDef<UserDoc>[] = [
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "name",
+    header: "Naam",
+    accessorFn: (row) => {
+      const firstName = row.firstName;
+      const lastName = row.lastName;
+      return `${firstName} ${lastName}`;
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdAt"));
+      const formatted = date.toLocaleDateString("nl-NL", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+    header: ({ column }) => {
+      const isSorted = column.getIsSorted();
+      const SortedIcon = !isSorted ? ArrowUpDown : isSorted === "asc" ? SortAsc : SortDesc;
+      return (
+        <Button
+          className="float-end text-[length:inherit]"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Aangemaakt op
+          <SortedIcon className="ml-2 size-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const client = row.original;
+
+      return (
+        <DropdownMenu key={client._id}>
+          <DropdownMenuTrigger asChild className="float-end">
+            <Button variant="ghost" className="size-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Acties</DropdownMenuLabel>
+            {/*<DropdownMenuItem onClick={() => navigator.clipboard.writeText(client._id)}>*/}
+            {/*  Kopieer link*/}
+            {/*</DropdownMenuItem>*/}
+            <DropdownMenuSeparator />
+            <Link href={`/dashboard/clients/${client._id}`}>
+              <DropdownMenuItem>
+                <Eye className="mr-1 size-3" />
+                Bekijk klant
+              </DropdownMenuItem>
+            </Link>
+            <ConfirmDialog
+              className="w-full cursor-default rounded hover:bg-muted"
+              onConfirm={async () => {
+                const { message, status } = await deleteClient(client._id, client.email);
+                if (message) {
+                  if (status === ResponseStatus.error) {
+                    toast({ title: "error", description: message, variant: "destructive" });
+                    return;
+                  }
+
+                  toast({ title: message });
+                }
+              }}
+            >
+              <DropdownMenuItem className="pointer-events-none">
+                <Trash className="mr-1 size-3" />
+                Verwijderen
+              </DropdownMenuItem>
+            </ConfirmDialog>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
