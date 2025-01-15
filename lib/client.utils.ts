431b2debdd21f5d6783d901abccf73ffbc131de4
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-15 03:25:57 +0100

    Added ConditionalWrap component to client.utils.ts

--- lib/client.utils.ts
+++ lib/client.utils.ts
@@ -12,3 +12,13 @@ export const handleAction = (
   const formData = new FormData(e.currentTarget);
   startTransition(() => action(formData)); // use onSubmit instead of action to retain the form state
 };
+
+export const ConditionalWrap = ({
+  condition,
+  wrap,
+  children,
+}: {
+  condition: boolean;
+  wrap: (children: React.ReactNode) => React.ReactNode;
+  children: React.ReactNode;
+}) => (condition ? wrap(children) : children);
