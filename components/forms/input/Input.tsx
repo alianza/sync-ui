af619fd1b9c98389aa0a8af24ad9678c1bc61ded
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-15 03:26:51 +0100

    Simplify input component

--- components/forms/input/Input.tsx
+++ components/forms/input/Input.tsx
@@ -3,18 +3,13 @@ import { Input as ShadCnInput } from "@/components/ui/input";
 import { Textarea as ShadCnTextArea, TextareaProps } from "@/components/ui/textarea";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { ConditionalWrap } from "@/lib/client.utils";
 
 interface Props {
   id: string;
   name?: string;
   type?: React.InputHTMLAttributes<HTMLInputElement>["type"] | "multiline";
-  placeholder?: string;
-  autoComplete?: React.InputHTMLAttributes<HTMLInputElement>["autoComplete"];
-  autoCapitalize?: React.InputHTMLAttributes<HTMLInputElement>["autoCapitalize"];
-  autoCorrect?: React.InputHTMLAttributes<HTMLInputElement>["autoCorrect"];
-  required?: boolean;
   label?: string;
-  minLength?: number;
   suffix?: string | React.ReactNode;
   hideSuffixOnHover?: boolean;
 }
@@ -23,83 +18,49 @@ export function Input({
   id,
   name,
   type = "text",
-  placeholder,
-  autoComplete = "off",
-  autoCapitalize = "none",
-  autoCorrect = "off",
-  required = false,
   label,
-  minLength,
   suffix,
-  hideSuffixOnHover,
+  hideSuffixOnHover = true,
   ...props
 }: Props & React.ComponentProps<"input"> & TextareaProps & React.RefAttributes<HTMLTextAreaElement>) {
   const InputTag = type === "multiline" ? ShadCnTextArea : ShadCnInput;
 
   return (
-    <LabelWrap htmlFor={id} label={label} required={required}>
-      <InputSuffixWrap suffix={suffix} hideSuffixOnHover={hideSuffixOnHover}>
+    <ConditionalWrap
+      condition={!!label}
+      wrap={(children) => (
+        <Label htmlFor={id} className="group/label flex flex-col gap-1 text-sm font-medium text-foreground">
+          <span>
+            {label}
+            {props.required && <span className="text-muted-foreground">*</span>}
+          </span>
+          {children}
+        </Label>
+      )}
+    >
+      <ConditionalWrap
+        condition={!!suffix}
+        wrap={(children) => (
+          <div className="group relative">
+            {children}
+            <div
+              className={cn("absolute inset-y-0 right-0 flex items-center pr-2.5 text-sm", {
+                "pointer-events-none group-hover/label:hidden group-hover:hidden": hideSuffixOnHover,
+              })}
+            >
+              {suffix}
+            </div>
+          </div>
+        )}
+      >
         <InputTag
           id={id}
           name={id || name}
-          placeholder={placeholder}
           type={type}
-          autoCapitalize={autoCapitalize}
-          autoComplete={autoComplete}
-          autoCorrect={autoCorrect}
-          required={required}
-          minLength={minLength}
           className={cn("hover:ring-1 hover:ring-accent", props.className)}
           {...props}
         />
-      </InputSuffixWrap>
-    </LabelWrap>
-  );
-}
-
-function InputSuffixWrap({
-  children,
-  suffix,
-  hideSuffixOnHover = true,
-}: {
-  children: React.ReactNode;
-  suffix?: string | React.ReactNode;
-  hideSuffixOnHover?: boolean;
-}) {
-  if (!suffix) return children;
-  return (
-    <div className="group relative">
-      {children}
-      <div
-        className={cn("absolute inset-y-0 right-0 flex items-center pr-2.5 text-sm", {
-          "pointer-events-none group-hover/label:hidden group-hover:hidden": hideSuffixOnHover,
-        })}
-      >
-        {suffix}
-      </div>
-    </div>
-  );
-}
-
-function LabelWrap({
-  htmlFor,
-  label,
-  required,
-  children,
-}: {
-  htmlFor: string;
-  label?: string;
-  required: boolean;
-  children: React.ReactNode;
-}) {
-  if (!label) return children;
-  return (
-    <Label htmlFor={htmlFor} className="group/label flex flex-col gap-1 text-sm font-medium text-foreground">
-      <span>
-        {label}
-        {required && <span className="text-muted-foreground">*</span>}
-      </span>
-      {children}
-    </Label>
+      </ConditionalWrap>
+    </ConditionalWrap>
   );
 }
