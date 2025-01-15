10e009a937aca7c45c1bfd42ccc3b1d5ee94215f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-01-15 03:07:06 +0100

    Translated actions and added RejectInviteAction in invite actions.ts

--- app/(app)/invite/actions.ts
+++ app/(app)/invite/actions.ts
@@ -23,7 +23,7 @@ const acceptInviteSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
   confirmPassword: z.string().min(8),
-  inviteId: z.string().refine(ObjectId.isValid, { message: "Invalid invite ID" }),
+  inviteId: z.string().refine(ObjectId.isValid, { message: "Ongeldig uitnodiging id" }),
 });
 
 export async function AcceptInviteAction(prevState: unknown, formData: FormData) {
@@ -32,7 +32,7 @@ export async function AcceptInviteAction(prevState: unknown, formData: FormData)
       Object.fromEntries(formData),
     );
 
-    if (password !== confirmPassword) return failResponse({ message: "Passwords do not match" });
+    if (password !== confirmPassword) return failResponse({ message: "Wachtwoorden komen niet overeen" });
 
     const hashedPassword = await saltAndHashPassword(password);
 
@@ -43,21 +43,51 @@ export async function AcceptInviteAction(prevState: unknown, formData: FormData)
       { $set: { status: STATUS_ENUM.ACCEPTED, acceptedAt: new Date() } },
     ).populate<UserDoc>("inviter");
 
-    if (!invite) return failResponse({ message: "Invite not found" });
+    if (!invite) return failResponse({ message: "Uitnodiging niet gevonden" });
 
     const user = await User.create({ firstName, lastName, email, password: hashedPassword, role: ROLES.BUYER });
-
     await User.updateOne({ _id: invite.inviter._id }, { $push: { clients: user._id } });
 
     // return redirect("/login");
-    return successResponse({ message: "Successfully accepted invite!", data: serializeDoc(user) });
+    return successResponse({ message: "Uitnodiging succesvol geaccepteerd!", data: serializeDoc(user) });
   } catch (error) {
     if (error instanceof z.ZodError) {
       return failResponse({ message: formatZodError(error) });
     }
 
     if (isMongooseDuplicateKeyError(error)) {
-      return failResponse({ message: "This email is already in use, please log in" });
+      return failResponse({ message: "E-mailadres is al in gebruik, log in of gebruik een ander e-mailadres" });
+    }
+
+    return errorResponse(error);
+  }
+}
+
+export async function RejectInviteAction({ inviteeEmail, inviteID }: { inviteeEmail: string; inviteID: string }) {
+  try {
+    const { email, inviteId } = z
+      .object({ email: z.string().email(), inviteId: z.string().refine(ObjectId.isValid) })
+      .parse({
+        email: inviteeEmail,
+        inviteId: inviteID,
+      });
+
+    await dbConnect();
+
+    const invite = await ClientInvite.findOneAndUpdate<ClientInviteDoc>(
+      { _id: inviteId, status: STATUS_ENUM.PENDING, inviteeEmail: email },
+      // { $set: { status: STATUS_ENUM.REJECTED } },
+      {},
+    );
+
+    console.log(`invite`, invite);
+
+    if (!invite) return failResponse({ message: "Uitnodiging niet gevonden" });
+
+    return successResponse({ message: "Uitnodiging succesvol afgewezen" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return failResponse({ message: formatZodError(error) });
     }
 
     return errorResponse(error);
