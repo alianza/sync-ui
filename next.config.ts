dd9c02ec98d811f40f393c1318c03be113b013d6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-02-26 01:13:26 +0100

    Added react compiler and some components + update shadcn cli config components.json

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true, // Not working with TanStack React Table pagination and sorting
+  },
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   },
