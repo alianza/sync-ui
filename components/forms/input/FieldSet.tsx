718d0fb1463f6d96869159d9d922a5088a46545e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-03-19 02:00:05 +0100

    Update form FieldSet.tsx

--- components/forms/input/FieldSet.tsx
+++ components/forms/input/FieldSet.tsx
@@ -7,7 +7,7 @@ interface FieldSetProps {
 
 export function FieldSet({ label, children, ...props }: FieldSetProps) {
   return (
-    <fieldset className="flex flex-col gap-4 rounded-md border border-input p-2" {...props}>
+    <fieldset className="border-input flex w-full flex-col gap-4 self-start rounded-md border p-2" {...props}>
       {label && <legend className="text-sm font-bold">{label}</legend>}
       {children}
     </fieldset>
